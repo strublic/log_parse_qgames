e0ec80dfd55a3ca584cc9e9b6eea5a21b4dbdbc6
Gleds Strublic <gc.strublic@gmail.com>
2023-08-07 00:31:22 -0300

    first commit

---
+++ lib/log_parser.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "log_parser/version"
+
+module LogParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/log_parser/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LogParser
+  VERSION = "0.1.0"
+end
